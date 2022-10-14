06e79c174e67e245ad16655dfe89fdae7c3312df
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2022-10-14 22:30:58 +0200

    Fix create_session_id()
    - the problem was that the device id was not updated and thus every new row became a new session id
    - this should work faster as we don't iterate over all rows
    - write new csv file at end of vorbereitung.py
    - include speedfilter as a variable in read data to be flexible on data read in

--- analysis/vorbereitung.py
+++ analysis/vorbereitung.py
@@ -5,18 +5,31 @@
 *author: Janosch Walde
 """
 
-import os
-import pandas as pd
-import pytz
 import meteohautnah.meteohautnah as mh
+import pandas as pd
+from tqdm import tqdm
 
 if __name__ == '__main__':
     # %% define paths
-    data_path = "/home/janosch/Dokumente/MeteorologieHautnah/Daten/processed/"
-    plot_path = "/home/janosch/Dokumente/MeteorologieHautnah/Daten/plots/"
-    date = "2022-05-18"  # "2022-05-18"  # yyyy-mm-dd or all
-    # %% read in data and create session id
-    df = mh.read_data(data_path, date)
-    print(df)
-    mh.create_session_id(df)
-    print(df[['time', 'device_id', 'session_id']])
+    base_dir = "C:/Users/Johannes/Documents/MeteorologieHautnah/MeteorologieHautnah"
+    # base_dir = "/home/janosch/Dokumente/MeteorologieHautnah"  # Janosch
+    data_path = f"{base_dir}/Daten/processed/"
+    output_path = f"{base_dir}/Daten/v1.0"
+    plot_path = f"{base_dir}/Daten/plots/"
+    date = "2022-05-03"  # "2022-05-18"  # yyyy-mm-dd or all
+    dates = list(pd.date_range("2022-05-01", "2022-10-13").strftime("%Y-%m-%d"))
+    # %% read in data, create session id(test for one date)
+    # df = mh.read_data(data_path, date, speedfilter=0)
+    # print(df)
+    # df_new = mh.create_session_id(df)
+    # print(df_new[['time', 'device_id', 'session_id']])
+
+    # %% create session id for whole dataset and write to new csv file
+    for d in tqdm(dates):
+        try:
+            df = mh.read_data(data_path, d, speedfilter=0)
+        except IndexError:
+            print(f"No file found for {d}")
+            continue  # Probably no file for this date
+        df = mh.create_session_id(df)
+        df.to_csv(f"{output_path}/{d}_meteotracker.csv", index=False)
--- meteohautnah/meteohautnah.py
+++ meteohautnah/meteohautnah.py
@@ -4,17 +4,20 @@
 *author*: Johannes Röttenbacher, Janosch Walde
 """
 import os
+import numpy as np
 import pandas as pd
 import pytz
+from wetterdienst.provider.dwd.observation import DwdObservationRequest, DwdObservationDataset, DwdObservationPeriod, DwdObservationResolution
 
 
-def read_data(path: str, date: str) -> pd.DataFrame:
+def read_data(path: str, date: str, speedfilter: float) -> pd.DataFrame:
     """
     Read in data, filter for values > 10km/h and convert time from UTC to local time.
 
     Args:
         path: path where to find csv files
         date: "all" or "yyyy-mm-dd"
+        speedfilter: minimum speed of measurement to be kept in dataframe (km/h)
 
     Returns: pandas Dataframe with columns
 
@@ -26,8 +29,8 @@ def read_data(path: str, date: str) -> pd.DataFrame:
         file = [f for f in files if date in f][0]
         df = pd.read_csv(f"{path}/{file}")
 
-    # %% filter out values with speed below 10 km/h
-    df = df[df.speed > 10]
+    # %% filter out values with speed below speedfilter km/h
+    df = df[df.speed > speedfilter]
 
     # %% Convert time to datetime and to European timezone
     df.loc[:, "time"] = pd.to_datetime(df["time"])  # convert time column to type datetime
@@ -49,22 +52,56 @@ def create_session_id(df: pd.DataFrame, delta_t_min: float = 300) -> pd.DataFram
 
     """
     session_id = 0
-    df.sort_values(["device_id", "time"],
-                   ascending=True,
-                   inplace=True, )
-    dev_id = df.device_id[0]
-    t = df.iloc[0, 4]
-    df['session_id'] = None
-    for i in range(len(df)):
-        delta_t = df.iloc[i, 4] - t
-        delta_in_s = delta_t.total_seconds()
-        # gleiches Gerät und Zeitunterschied kleiner als 5 min
-        if df.iloc[i, 8] == dev_id and delta_in_s <= delta_t_min:
-            df.iloc[i, 12] = session_id
-            t = df.iloc[i, 4]
-        else:
+    dev_ids = df.device_id.unique()  # find unique device ids
+    df.sort_values(["device_id", "time"], ascending=True, inplace=True)
+    df["time_diff"] = df.time.diff()  # add column with time difference
+    df = df[df.time_diff != pd.to_timedelta(0)].copy()  # remove duplicated measurements and make sure a copy is returned
+    df['session_id'] = None  # add new column for session id
+    df_out = pd.DataFrame()
+    for dev_id in dev_ids:
+        df_tmp = df.loc[df.device_id == dev_id, :].copy()  # select only one device id and return copy
+        # get indices with time_diff >= delta_t_min
+        idx = np.nonzero(np.asarray(df_tmp["time_diff"] > pd.to_timedelta(delta_t_min, unit="s")))[0]
+        idx = np.insert(idx, 0, 0)  # add zero to beginning of indices
+        idx = np.append(idx, len(df_tmp))  # add last index
+        for i in range(len(idx)-1):
+            df_tmp.iloc[slice(idx[i], idx[i+1]), -1] = session_id
             session_id += 1
-            df.iloc[i, 12] = session_id
-            t = df.iloc[i, 4]
 
-    return df
+        df_out = pd.concat([df_out, df_tmp])
+        df_out.reset_index(drop=True, inplace=True)
+
+    return df_out
+
+
+def station_temp(name, start_date, end_date):
+    request = DwdObservationRequest(parameter=DwdObservationDataset.TEMPERATURE_AIR,
+                                    resolution=DwdObservationResolution.MINUTE_10,
+                                    start_date=start_date,
+                                    end_date=end_date,
+                                    ).filter_by_name(name=name)
+
+    df_res = request.values.all().df.dropna()
+
+    df_Temp = df_res[df_res.parameter == "temperature_air_mean_200"].drop(['dataset', 'parameter', 'quality'], axis=1)
+    df_Temp.rename(columns={'value': 'T'}, inplace=True)
+    df_dew = df_res[df_res.parameter == "temperature_dew_point_mean_200"].drop(
+        ['station_id', 'dataset', 'parameter', 'quality'], axis=1)
+
+    df_dew.rename(columns={'value': 'Td'}, inplace=True)
+
+    df_Temp.set_index(pd.DatetimeIndex(df_Temp['date']), inplace=True)
+
+    df_dew.set_index(pd.DatetimeIndex(df_Temp['date']), inplace=True)
+
+    df_out = df_Temp.merge(df_dew, how='left', left_index=True, right_index=True)
+    df_out["time"] = pd.to_datetime(df_Temp.date, format="%Y-%m-%d %H:%M:%S%z").dt.tz_localize(None)
+    # df_out["SEC"]=pd.to_timedelta(df_Temp.date).dt.total_seconds()
+    df_out.set_index(df_out["time"], inplace=True)
+    # df_out.drop(["date"], axis=1)
+    # df_out.set_index("time", inplace=True)
+    df_out = df_out.drop(["date_x", "date_y"], axis=1)
+    df_out["T"] = df_out["T"] - 273.15
+    df_out["Td"] = df_out["Td"] - 273.15
+
+    return df_out
