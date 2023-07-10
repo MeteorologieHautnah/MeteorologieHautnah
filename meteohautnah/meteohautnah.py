fdb49087dbff4316b441a5a80f98e50e67f96048
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2023-07-11 00:07:12 +0200

    Comment out wetterdienst and adapt read in

--- meteohautnah/meteohautnah.py
+++ meteohautnah/meteohautnah.py
@@ -7,7 +7,7 @@ import os
 import numpy as np
 import pandas as pd
 import pytz
-from wetterdienst.provider.dwd.observation import DwdObservationRequest, DwdObservationDataset, DwdObservationResolution
+# from wetterdienst.provider.dwd.observation import DwdObservationRequest, DwdObservationDataset, DwdObservationResolution
 
 
 def read_data(path: str, date: str, speedfilter: float) -> pd.DataFrame:
@@ -33,7 +33,7 @@ def read_data(path: str, date: str, speedfilter: float) -> pd.DataFrame:
     df = df[df.speed > speedfilter]
 
     # %% Convert time to datetime and to European timezone
-    df.loc[:, "time"] = pd.to_datetime(df["time"])  # convert time column to type datetime
+    df["time"] = pd.to_datetime(df["time"], format="mixed")  # convert time column to type datetime
     my_timezone = pytz.timezone('Europe/Berlin')
     df['time'] = df['time'].dt.tz_convert(my_timezone)
 
@@ -74,37 +74,37 @@ def create_session_id(df: pd.DataFrame, delta_t_min: float = 300) -> pd.DataFram
     return df_out
 
 
-def station_temp(name, start_date, end_date):
-    request = DwdObservationRequest(parameter=DwdObservationDataset.TEMPERATURE_AIR,
-                                    resolution=DwdObservationResolution.MINUTE_10,
-                                    start_date=start_date,
-                                    end_date=end_date,
-                                    ).filter_by_name(name=name)
-
-    df_res = request.values.all().df.dropna()
-
-    df_Temp = df_res[df_res.parameter == "temperature_air_mean_200"].drop(['dataset', 'parameter', 'quality'], axis=1)
-    df_Temp.rename(columns={'value': 'T'}, inplace=True)
-    df_dew = df_res[df_res.parameter == "temperature_dew_point_mean_200"].drop(
-        ['station_id', 'dataset', 'parameter', 'quality'], axis=1)
-
-    df_dew.rename(columns={'value': 'Td'}, inplace=True)
-
-    df_Temp.set_index(pd.DatetimeIndex(df_Temp['date']), inplace=True)
-
-    df_dew.set_index(pd.DatetimeIndex(df_Temp['date']), inplace=True)
-
-    df_out = df_Temp.merge(df_dew, how='left', left_index=True, right_index=True)
-    df_out["time"] = pd.to_datetime(df_Temp.date, format="%Y-%m-%d %H:%M:%S%z").dt.tz_localize(None)
-    # df_out["SEC"]=pd.to_timedelta(df_Temp.date).dt.total_seconds()
-    df_out.set_index(df_out["time"], inplace=True)
-    # df_out.drop(["date"], axis=1)
-    # df_out.set_index("time", inplace=True)
-    df_out = df_out.drop(["date_x", "date_y"], axis=1)
-    df_out["T"] = df_out["T"] - 273.15
-    df_out["Td"] = df_out["Td"] - 273.15
-
-    return df_out
+# def station_temp(name, start_date, end_date):
+#     request = DwdObservationRequest(parameter=DwdObservationDataset.TEMPERATURE_AIR,
+#                                     resolution=DwdObservationResolution.MINUTE_10,
+#                                     start_date=start_date,
+#                                     end_date=end_date,
+#                                     ).filter_by_name(name=name)
+#
+#     df_res = request.values.all().df.dropna()
+#
+#     df_Temp = df_res[df_res.parameter == "temperature_air_mean_200"].drop(['dataset', 'parameter', 'quality'], axis=1)
+#     df_Temp.rename(columns={'value': 'T'}, inplace=True)
+#     df_dew = df_res[df_res.parameter == "temperature_dew_point_mean_200"].drop(
+#         ['station_id', 'dataset', 'parameter', 'quality'], axis=1)
+#
+#     df_dew.rename(columns={'value': 'Td'}, inplace=True)
+#
+#     df_Temp.set_index(pd.DatetimeIndex(df_Temp['date']), inplace=True)
+#
+#     df_dew.set_index(pd.DatetimeIndex(df_Temp['date']), inplace=True)
+#
+#     df_out = df_Temp.merge(df_dew, how='left', left_index=True, right_index=True)
+#     df_out["time"] = pd.to_datetime(df_Temp.date, format="%Y-%m-%d %H:%M:%S%z").dt.tz_localize(None)
+#     # df_out["SEC"]=pd.to_timedelta(df_Temp.date).dt.total_seconds()
+#     df_out.set_index(df_out["time"], inplace=True)
+#     # df_out.drop(["date"], axis=1)
+#     # df_out.set_index("time", inplace=True)
+#     df_out = df_out.drop(["date_x", "date_y"], axis=1)
+#     df_out["T"] = df_out["T"] - 273.15
+#     df_out["Td"] = df_out["Td"] - 273.15
+#
+#     return df_out
 
 
 def session_change(s):
